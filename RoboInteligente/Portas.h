5f7143b368528076d023444f21bd995351be7d8e
Arthur Carlos <arthurcarlos.faria@gmail.com>
2022-09-01 13:11:50 -0300

    Versão 01-09-2022

--- RoboInteligente/Portas.h
+++ RoboInteligente/Portas.h
@@ -2,21 +2,23 @@
 
 #define pino_trigger 22
 #define pino_echo 24
-
+#include <Adafruit_MPU6050.h>
+Adafruit_MPU6050 mpu;
 Ultrasonic ultrasonic(pino_trigger, pino_echo);
 Servo servos[12];
+
 long int tempo = 0;
 
 
 int estadosMotores[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //Estados atuais dos motores
 int comandosMotores[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //Comandos atuais para os motores
-float sensores[7] = {0, 0, 0, 0, 0, 0,0 }; //Medicoes dos sensores{EIX,EIY,EIZ,EAX,EAY,EAZ,ED}
+float sensores[8] = {0, 0, 0, 0, 0, 0,0,0 }; //Medicoes dos sensores{EIX,EIY,EIZ,EAX,EAY,EAZ,ED}
 int delayMotores[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //Comandos atuais para os motores
 //MÉTODO RESPONSÁVEL POR CALCULAR A DISTÂNCIA USANDO O SENSOR ULTRASSONICO HCSR-04
 void hcsr04() {
 
   long microsec = ultrasonic.timing();
-  sensores[6] = ultrasonic.convert(microsec, Ultrasonic::CM);
+  sensores[7] = ultrasonic.convert(microsec, Ultrasonic::CM);
 
 }
 void lerServos(){
@@ -34,28 +36,60 @@ const int MPU=0x68;
 void lerGiroscopioAcelerometro(){
   //Endereco I2C do MPU6050
 
-  int AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+ // int AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
   //Solicita os dados do sensor
   Wire.requestFrom(MPU,14,true);  
   //Armazena o valor dos sensores nas variaveis correspondentes
-  AcX=Wire.read()<<8|Wire.read();  //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-  AcY=Wire.read()<<8|Wire.read();  //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ=Wire.read()<<8|Wire.read();  //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  //0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX=Wire.read()<<8|Wire.read();  //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY=Wire.read()<<8|Wire.read();  //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ=Wire.read()<<8|Wire.read();  //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+//  AcX=Wire.read()<<8|Wire.read();  //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+//  AcY=Wire.read()<<8|Wire.read();  //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+//  AcZ=Wire.read()<<8|Wire.read();  //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+//  Tmp=Wire.read()<<8|Wire.read();  //0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+//  GyX=Wire.read()<<8|Wire.read();  //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+//  GyY=Wire.read()<<8|Wire.read();  //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+//  GyZ=Wire.read()<<8|Wire.read();  //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
    
+
+//Inicio leitura
+
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+//  /* Print out the values */
+// // Serial.print("Acceleration X: ");
+//  Serial.print(a.acceleration.x);
+// // Serial.print(", Y: ");
+//  Serial.print(a.acceleration.y);
+////  Serial.print(", Z: ");
+//  Serial.print(a.acceleration.z);
+// // Serial.println(" m/s^2");
+
+//  Serial.print("Rotation X: ");
+//  Serial.print(g.gyro.x);
+//  Serial.print(", Y: ");
+//  Serial.print(g.gyro.y);
+//  Serial.print(", Z: ");
+//  Serial.print(g.gyro.z);
+//  Serial.println(" rad/s");
+//
+//  Serial.print("Temperature: ");
+//  Serial.print(temp.temperature);
+//  Serial.println(" degC");
+//
+//  Serial.println("");
+//  delay(500);
+//fim
  
-  sensores[0]=float(GyX);
-  sensores[1]=float(GyY);
-  sensores[2]=float(GyZ);
-  sensores[3]=float(AcX);
-  sensores[4]=float(AcY);
-  sensores[5]=float(AcZ);
+  sensores[0]=g.gyro.x;
+  sensores[1]=g.gyro.y;
+  sensores[2]=g.gyro.z;
+  sensores[3]=a.acceleration.x;
+  sensores[4]=a.acceleration.y;
+  sensores[5]=a.acceleration.z;
+  sensores[6]=temp.temperature;
  
   //Aguarda 300 ms e reinicia o processo
  // delay(300);
--- RoboInteligente/RoboInteligente.ino
+++ RoboInteligente/RoboInteligente.ino
@@ -10,7 +10,7 @@ long int contaLoops=0;
     
     
     void printCabecalho(){
-      Serial.println("EM1 EM2 EM3 EM4 EM5 EM6 EM7 EM8 EM9 EM10 EM11 EM12 CM1 CM2 CM3 CM4 CM5 CM6 CM7 CM8 CM9 CM10 CM11 CM12 AcX AcY AcZ Tmp GyX GyY GyZ HCSR ");
+      Serial.println("EM1 EM2 EM3 EM4 EM5 EM6 EM7 EM8 EM9 EM10 EM11 EM12 CM1 CM2 CM3 CM4 CM5 CM6 CM7 CM8 CM9 CM10 CM11 CM12 AcX AcY AcZ  GyX GyY GyZ HCSR Tmp ");
     }
     void print() {
       Serial.print(this->contaLoops);
@@ -25,7 +25,7 @@ long int contaLoops=0;
         Serial.print(comandosMotores[i]);
         Serial.print(" ");
       }
-      for (int i = 0; i < 7; i++) {
+      for (int i = 0; i < 8; i++) {
         Serial.print(sensores[i]);
         Serial.print(" ");
       }
@@ -75,7 +75,9 @@ Wire.write(0x6B);
 //Inicializa o MPU-6050
 Wire.write(0);
 Wire.endTransmission(true);
-
+if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+}
    servos[0].attach(2);  // attaches the servo on pin
 
    servos[1].attach(3);  // attaches the servo on pin 
@@ -101,7 +103,67 @@ Wire.endTransmission(true);
    servos[11].attach(13);  // attaches the servo on pin 
    
 
+mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
 
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
   Serial.println("Robo Inteligente Iniciado");
   modoColherDados.Setup();
 }
